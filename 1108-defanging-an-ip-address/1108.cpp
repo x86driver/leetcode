f0c648e93684e54a6b4378c25ded328afd62283c
Shane Lin <253692+doremi@users.noreply.github.com>
2022-09-12 16:21:14 +0800

    1108. Defanging an IP Address

---
+++ 1108-defanging-an-ip-address/1108.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ret;
+        for (char c : address) {
+            if (c == '.') {
+                ret += "[.]";
+            } else {
+                ret += c;
+            }
+        }
+
+        return ret;
+    }
+};
