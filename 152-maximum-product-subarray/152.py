66dac367583d43f4dcb1bcae8d47552d5b100001
Shane Lin <253692+doremi@users.noreply.github.com>
2022-10-25 14:42:52 +0800

    152. Maximum Product Subarray

---
+++ 152-maximum-product-subarray/152.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+            max_so_far = temp_max
+
+            result = max(result, max_so_far)
+
+        return result
