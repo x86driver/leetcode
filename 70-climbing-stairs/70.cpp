78dbd405c6195a2f4ad70e1a8195362ebcf4f2b8
Shane Lin <253692+doremi@users.noreply.github.com>
2022-12-23 08:58:36 +0800

    70. Climbing Stairs

---
+++ 70-climbing-stairs/70.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int *dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n+1; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        int ans = dp[n];
+        delete [] dp;
+
+        return ans;
+    }
+};
