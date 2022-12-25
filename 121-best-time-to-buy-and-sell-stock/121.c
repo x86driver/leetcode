41505e041a61200850aafd0e69c7cbea135c87e1
Shane Lin <253692+doremi@users.noreply.github.com>
2022-12-25 09:32:08 +0800

    121. Best Time to Buy and Sell Stock
    * C

---
+++ 121-best-time-to-buy-and-sell-stock/121.c
@@ -0,0 +1,18 @@
+#define max(x, y) (((x)) > ((y)) ? ((x)) : ((y)))
+
+int maxProfit(int* prices, int pricesSize){
+    int curr_min = 10000;
+    int curr_max = 0;
+    int profit = 0;
+
+    for (int i = 0; i < pricesSize; ++i) {
+        if (prices[i] < curr_min) {
+            curr_min = curr_max = prices[i];
+        } else if (prices[i] > curr_max) {
+            curr_max = prices[i];
+            profit = max(profit, curr_max - curr_min);
+        }
+    }
+
+    return profit;
+}
