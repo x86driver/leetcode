423659933163ac33b6545bc38253d38f2149beae
Shane Lin <253692+doremi@users.noreply.github.com>
2023-01-27 19:21:24 +0800

    704. Binary Search

---
+++ 704-binary-search/704.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
