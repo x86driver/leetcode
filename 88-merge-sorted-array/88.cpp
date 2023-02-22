888caaa7908fa85ec68a209ae0776baad533a459
Shane Lin <253692+doremi@users.noreply.github.com>
2023-02-22 15:13:45 +0800

    88. Merge Sorted Array

---
+++ 88-merge-sorted-array/88.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m + n - 1;
+
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[i--] = nums1[--m];
+            } else {
+                nums1[i--] = nums2[--n];
+            }
+        }
+
+        while (n > 0)
+            nums1[i--] = nums2[--n];
+    }
+};
