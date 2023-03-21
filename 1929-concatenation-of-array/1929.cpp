325c7458b1d21dd58afb8efa23f95f8c6be75920
Shane Lin <253692+doremi@users.noreply.github.com>
2023-03-21 21:54:37 +0800

    1929. Concatenation of Array

---
+++ 1929-concatenation-of-array/1929.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(nums.size() * 2);
+        for (size_t i = 0; i < nums.size(); ++i) {
+            ans[i] = nums[i];
+            ans[i + nums.size()] = nums[i];
+        }
+        return ans;
+    }
+};
