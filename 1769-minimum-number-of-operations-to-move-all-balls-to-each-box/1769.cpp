ed90ebaae73293821cb39471daf3a711b5152cb7
Shane Lin <253692+doremi@users.noreply.github.com>
2023-06-04 09:37:08 +0800

    1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> ans;
+        for (int i = 0; i < boxes.size(); ++i) {
+            int op = 0;
+            for (int j = 0; j < boxes.size(); ++j) {
+                if (i == j)
+                    continue;
+                if (boxes[j] == '1') {
+                    op += abs(j - i);
+                }
+            }
+            ans.push_back(op);
+        }
+        return ans;
+    }
+};
