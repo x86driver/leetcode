0c4b7049cce84b6f71792c3a057d0f3f6492b1a8
Shane Lin <253692+doremi@users.noreply.github.com>
2023-07-15 07:44:02 +0800

    53. Maximum Subarray

---
+++ 53-maximum-subarray/53-greedy.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            count += nums[i];
+            if (count > result)
+                result = count;
+            if (count <= 0)
+                count = 0;
+        }
+        return result;
+    }
+};
