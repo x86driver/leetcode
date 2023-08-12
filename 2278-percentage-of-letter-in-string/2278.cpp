487b80b21d4b05b0c99d45950e1be95d45ee5234
Shane Lin <253692+doremi@users.noreply.github.com>
2023-08-12 08:40:53 +0800

    2278. Percentage of Letter in String

---
+++ 2278-percentage-of-letter-in-string/2278.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count = 0;
+        for (char c : s)
+            if (c == letter)
+                ++count;
+        return (count * 1.0) / s.size() * 100;
+    }
+};
