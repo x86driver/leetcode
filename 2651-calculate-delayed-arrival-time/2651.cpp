b1a4f5dc667ca5c811f418e6cb81d008fb546bc0
Shane Lin <253692+doremi@users.noreply.github.com>
2024-06-18 15:25:42 +0800

    2651. Calculate Delayed Arrival Time

---
+++ 2651-calculate-delayed-arrival-time/2651.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int ans = arrivalTime + delayedTime;
+        if (ans >= 24)
+            return ans - 24;
+        else
+            return ans;
+    }
+};
