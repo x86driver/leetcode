ff680025b3806351c5b755101052b684746b2820
Shane Lin <253692+doremi@users.noreply.github.com>
2024-07-06 05:18:30 +0800

    2363. Merge Similar Items

---
+++ 2363-merge-similar-items/2363-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        vector<int> m(1001);
+        for (const vector<int> item : items1) {
+            m[item[0]] = item[1];
+        }
+        for (const vector<int> item : items2) {
+            m[item[0]] += item[1];
+        }
+        vector<vector<int>> ans;
+        for (int i = 1; i < m.size(); ++i) {
+            if (m[i] > 0)
+                ans.push_back({i, m[i]});
+        }
+        return ans;
+    }
+};
