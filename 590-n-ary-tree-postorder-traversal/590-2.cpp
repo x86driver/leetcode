0141ed06e67431a6ed7ec932bfbe930547a124b8
Shane Lin <253692+doremi@users.noreply.github.com>
2024-11-26 12:24:14 +0800

    590. N-ary Tree Postorder Traversal

---
+++ 590-n-ary-tree-postorder-traversal/590-2.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+private:
+    vector<int> ans;
+    void dfs(Node* root) {
+        if (!root)
+            return;
+        for (Node* node : root->children) {
+            dfs(node);
+        }
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorder(Node* root) {
+        dfs(root);
+        return ans;
+    }
+};
