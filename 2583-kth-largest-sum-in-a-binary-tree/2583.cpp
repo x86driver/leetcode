06be9404e3f6295e33bc14453d22045727af9966
Shane Lin <253692+doremi@users.noreply.github.com>
2024-11-27 10:59:18 +0800

    2583. Kth Largest Sum in a Binary Tree

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        priority_queue<long long> pq;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            long long sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            pq.push(sum);
+        }
+
+        if (k > pq.size())
+            return -1;
+
+        while (--k) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
