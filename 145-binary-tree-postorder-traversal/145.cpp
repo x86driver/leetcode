1a368d14eaba25da8c2ae67b3dcefb9cf3cb4db7
Shane Lin <253692+x86driver@users.noreply.github.com>
2024-12-27 16:52:26 +0800

    145. Binary Tree Postorder Traversal

---
+++ 145-binary-tree-postorder-traversal/145.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void dfs(TreeNode* root, vector<int>& ans) {
+        if (!root) {
+            return;
+        }
+        dfs(root->left, ans);
+        dfs(root->right, ans);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        ans.reserve(100);
+        dfs(root, ans);
+        return ans;
+    }
+};
