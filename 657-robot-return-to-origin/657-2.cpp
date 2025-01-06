0d836e932ed09c5a6a4a646488bacb88f8036047
Shane Lin <253692+doremi@users.noreply.github.com>
2025-01-06 10:44:51 +0800

    657. Robot Return to Origin

---
+++ 657-robot-return-to-origin/657-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for (char c : moves) {
+            switch (c) {
+                case 'U':
+                    --y;
+                    break;
+                case 'D':
+                    ++y;
+                    break;
+                case 'L':
+                    --x;
+                    break;
+                case 'R':
+                    ++x;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+};
