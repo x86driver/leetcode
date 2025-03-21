c29070dd845a705aba509d61bdabf583a284ee61
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-03-21 12:17:50 +0800

    2620. Counter

---
+++ 2620-counter/2620.ts
@@ -0,0 +1,15 @@
+function createCounter(n: number): () => number {
+    let counter = n;
+    return function() {
+        return n++;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+ 
