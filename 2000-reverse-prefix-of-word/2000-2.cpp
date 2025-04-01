013d8623fdd54766ac74a7d98278c9466ddef4fa
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-04-01 11:22:12 +0800

    2000. Reverse Prefix of Word

---
+++ 2000-reverse-prefix-of-word/2000-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index = -1;
+        for (int i = 0; i < word.size(); ++i) {
+            if (word[i] == ch) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            return word;
+        }
+
+        ++index;
+        for (int i = 0; i < index / 2; ++i) {
+            swap(word[i], word[index - i - 1]);
+        }
+        return word;
+    }
+};
