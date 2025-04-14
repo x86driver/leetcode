f36cd057799523315871be935849aef6ea48956a
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-04-14 15:12:43 +0800

    2697. Lexicographically Smallest Palindrome

---
+++ 2697-lexicographically-smallest-palindrome/2697.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+        while (start < end) {
+            if (s[start] < s[end]) {
+                s[end] = s[start];
+            } else {
+                s[start] = s[end];
+            }
+            ++start;
+            --end;
+        }
+        return s;
+    }
+};
