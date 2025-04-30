d509ec18097850dd7d06eb406d4d1e401bb4b10e
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-04-30 10:47:15 +0800

    168. Excel Sheet Column Title

---
+++ 168-excel-sheet-column-title/168.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans = "";
+        while (columnNumber > 0) {
+            --columnNumber;
+            int r = columnNumber % 26;
+            ans += (r + 'A');
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+
+        return "";
+    }
+};
