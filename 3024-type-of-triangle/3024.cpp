dcf34150e59ae83f5cff843d30875527a7d4b0c2
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-05-19 13:46:20 +0800

    3024. Type of Triangle

---
+++ 3024-type-of-triangle/3024.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) {
+            return "none";
+        }
+        if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) {
+            return "isosceles";
+        }
+        if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
+            return "scalene";
+        }
+        return "none";
+    }
+};
